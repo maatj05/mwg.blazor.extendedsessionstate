027319ea298fa453bedc09ebf925dfdb6a1bfa05
jacco@maatwerkgiethoorn.nl <jacco@maatwerkgiethoorn.nl>
2024-10-21 23:21:48 +0200

    truydy

--- Demo/RendrModeDemo.Client/AppState.cs
+++ Demo/RendrModeDemo.Client/AppState.cs
@@ -3,10 +3,6 @@
 
 public record AppState(Guid Id, bool IsCheckedOut, string Name, int CounterCount)
 {
-    public AppState() : this(Guid.NewGuid(), false, "Default", 0)
-    {
-
-    }
-
+     
 
 }
